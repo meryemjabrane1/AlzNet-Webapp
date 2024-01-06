002ccd3d7656715c46fcbf260dc4b0be346d710d
Meryem Jabrane <M.Jabrane@aui.ma>
2024-01-06 01:45:37 +0100

    Intiial Folder structure and first components

---
+++ alznet-frontend/src/App.js
@@ -0,0 +1,9 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
