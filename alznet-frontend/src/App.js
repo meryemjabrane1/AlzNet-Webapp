7d1776716c21e686e8668654de6587b07678cd1e
Meryem Jabrane <M.Jabrane@aui.ma>
2024-01-08 13:38:05 +0100

    update

--- alznet-frontend/src/App.js
+++ alznet-frontend/src/App.js
@@ -1,9 +1,40 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+/* import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import Calendar from "./scenes/calendar";
+ */
 
+ 
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode(); 
+  return (
+  <ColorModeContext.Provider value = {colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> 
+      <div className="app">
+        <Sidebar />
+        <main className ="content">
+          <Topbar />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            {/* <Route path="/bar" element={<Bar />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/line" element={<Line />} />
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/calendar" element={<Calendar />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
+  );
 }
-
+ 
 export default App;
--- alznet-frontend/src/scenes/global/Sidebar.jsx
+++ alznet-frontend/src/scenes/global/Sidebar.jsx
@@ -1,4 +1,48 @@
+import { useState } from 'react';
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import  { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedOutlinedIcon from "@mui/icons-material/PieChartOutlinedOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const Sidebar = () => {
-    return <div>Sidebar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [collapsed, setIsCollpased] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+
+// overriding part of the pro sidebar styling
+    return (
+        <Box 
+            sx = {{
+            "& .pro-sidebar-inner": {
+              background: '${colors.primary[400]} !important'
+            },
+            "& .pro-sidebar-wrapper": {
+                backgroundColor:  "transparent !important"
+            },
+            "& .pro-inner-item": {
+                padding: "5px 35px 5px 20px !important"
+            },
+            "& .pro-inner-item:hover": {
+                color: "#868dfb !important"
+            },
+            "& .pro-menu-item.active": {
+                color: "#6870fa !important"
+            },
+        }}
+        >
+        </Box>
+    );
 }
 export default Sidebar;
