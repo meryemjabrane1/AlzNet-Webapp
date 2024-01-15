ae871f7931c193bd4cab3da88fb84361551f879d
Meryem Jabrane <M.Jabrane@aui.ma>
2024-01-15 23:55:44 +0100

    creation and update calendar

---
+++ alznet-frontend/src/scenes/calendar/calendar.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+
