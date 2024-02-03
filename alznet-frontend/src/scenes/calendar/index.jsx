9fd14c408f3f05c33b00643b4aa6f38b387f6e23
Meryem Jabrane <M.Jabrane@aui.ma>
2024-02-03 23:48:20 +0100

    update calendar

--- alznet-frontend/src/scenes/calendar/index.jsx
+++ alznet-frontend/src/scenes/calendar/index.jsx
@@ -4,14 +4,24 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import {
+    Box, 
+    List, 
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme
+}from "@mui/material";
+import Header from "../../components/header";
+import { tokens } from "../../theme";
+
 
 const Calendar = () => {
-const theme = useTheme();
-const colors = tokens(theme.palette.mode);
 
-return {
-    
-}
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+return {}
 }
 
 export default Calendar;
